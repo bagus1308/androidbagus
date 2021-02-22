bfe269c8e1107e7ec28d5fb3b136fe9b422c685a
unknown <baguseka0898@gmail.com>
2021-02-22 11:33:22 -0800

    commit2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import List from './List';
+import Daftar from './List';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => List);
+AppRegistry.registerComponent(appName, () => Daftar);
